6cfc77fa58bd2c71df9967493ad7f8c2148d91aa
Genaro <gdimartino@devkingos.com>
2020-02-23 17:37:50 -0300

    firechat sin twitter

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAja6770S2RWmc-FCqGPzZLtKi7lU2Ew-M",
+    authDomain: "fire-chat-ng.firebaseapp.com",
+    databaseURL: "https://fire-chat-ng.firebaseio.com",
+    projectId: "fire-chat-ng",
+    storageBucket: "fire-chat-ng.appspot.com",
+    messagingSenderId: "879546809614",
+    appId: "1:879546809614:web:92fb10a5cff43c5886eb5f"
+  }
 };
 
 /*
@@ -14,3 +23,4 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
